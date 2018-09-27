3011ca73e7f96b36fc18db1f29b5cb96b989c581
Vinnys Lins <vlins@orion.tech>
2018-09-27 16:59:15 -0300

    GET endpoints for /users and /users/:id implemented

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const { User } = require('./app/models');
+const MESSAGES = {
+  serverError: 'Ocorreu um erro no servidor.',
+  codeNotEntered: 'Código não informado.'
+}
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -9,9 +13,28 @@ app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
-app.get('/users', (req, res) => {});
+app.get('/users', (req, res) => {
+  User.findAll().then(users => {
+    return res.json(users);
+  }).catch(() => {
+    return res.status(500).json({ message: MESSAGES.serverError });
+  });
+});
 
-app.get('/users/:id', (req, res) => {});
+app.get('/users/:id', (req, res) => {
+  const id = req.params.id;
+  if (!id) {
+    return res.status(400).json({ message: MESSAGES.codeNotEntered });
+  }
+  User.findById(id).then(user => {
+    if (!user) {
+      return res.status(404).json({ message: 'Usuário não encontrado.' });
+    }
+    return res.json(user);
+  }).catch(() => {
+    return res.status(500).json({ message: MESSAGES.serverError });
+  });
+});
 
 app.post('/users', async (req, res) => {
 	const user = await User.create(req.body);
